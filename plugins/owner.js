627421cd71c4adaaed5a508be0d398ff083ed54c
ACHULISA <103747284+ACHULISA@users.noreply.github.com>
2022-04-17 12:03:20 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*ʀᴜᴅʜʀᴀ ʙʏ ᴘʀɪɴᴄᴇ ʀᴜᴅʜ*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*ʀᴜᴅʜʀA BY ACHU SER*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
